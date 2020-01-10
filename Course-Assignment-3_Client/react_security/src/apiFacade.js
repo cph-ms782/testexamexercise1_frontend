660aad5c846f2f9ed0825566605a80205fd7c988
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2020-01-10 15:59:25 +0100

    CRUD hobbies works

--- Course-Assignment-3_Client/react_security/src/apiFacade.js
+++ Course-Assignment-3_Client/react_security/src/apiFacade.js
@@ -1,8 +1,5 @@
 import MAINURL from "./settings";
 
-//The two methods below, are the utility-methods introduced here (use them if you like):
-//https://docs.google.com/document/d/1hF9P65v_AJKCjol_gFkm3oZ1eVTuOKc15V6pcb3iFa8/edit?usp=sharing
-
 function makeOptions(method, addToken, body, loggedIn) {
   console.log('ApiFacade makeOptions');
   console.log('ApiFacade makeOptions method', method);
@@ -20,22 +17,11 @@ function makeOptions(method, addToken, body, loggedIn) {
     opts.headers['x-access-token'] = localStorage.getItem('jwtToken');
   }
   if (body) {
-    opts.body = body;
+    opts.body = JSON.stringify(body);
   }
   console.log('opts', opts);
   return opts;
 }
-
-// function makeOptionsCORS(method) {
-//   var opts = {
-//     method: method,
-//     headers: {
-//       "Access-Control-Request-Method": URL,
-//       "Access-Control-Request-Headers": "Content-Length,API-Key"
-//     }
-//   };
-//   return opts;
-// }
 
 function handleHttpErrors(res) {
   if (!res.ok) {
@@ -46,119 +32,38 @@ function handleHttpErrors(res) {
 
 function apiFacade() {
 
-  //OBSERVE This returns a promise, NOT the actual data, you must handle asynchronicity by the client
-  // async function getData() {
-  //   const options = makeOptionsCORS("OPTIONS");
-  //   const data = await fetch(URL, options).then(handleHttpErrors);
-  //   return data;
-  // }
-  function getData(url) {
-    console.log("apiFacade - getData");
-    console.log("MAINURL + url", MAINURL + url);
-    const data = Promise.resolve(fetch(MAINURL + url))
-      .then(handleHttpErrors)
-      .then(data => {
-        console.log("apifacade - getData data", data);
-        return data;
-      })
-      .catch(console.log.bind(console));
-    return data;
-  }
-
-  async function getDataAsync(url) {
-    console.log("apiFacade - getDataAsync");
-    const data = await fetch(url)
-      .then(handleHttpErrors)
-      .then(data => {
-        console.log("apiFacade - getDataAsync - data", data);
-        return data;
-      })
-      .catch(console.log.bind(console));
-    console.log("apiFacade - getDataAsync - data", data);
-    return data;
-  }
-
-  // function getData(url) {
-  //   console.log("apiFacade - getData");
-  //   const data = fetch(url)
-  //     .then(res => res.json())
-  //     .then(dat => {
-  //       console.log("apiFacade - getData - dat", dat);
-  //       return dat;
-  //     });
-  //   console.log("apiFacade - getData - data", data);
-  //   return data;
-  // }
-
-  // async function getDataAsync(url) {
-  //   console.log("apiFacade - getData");
-  //   const data = await fetch(url).then(handleHttpErrors);
-  //   console.log("apiFacade - getDataAsync - data", data);
-  //   return data;
-  // }
-
-  async function addEditItem(itemBody, loggedIn) {
+  async function addEditItem(itemBody, loggedIn, extraURL, id) {
     console.log("addEditItem itemBody", itemBody);
+
+    console.log("addEditItem id", id);
     console.log("addEditItem loggedIn", loggedIn);
 
-    if (itemBody.id === 0) {
+    if (id === 0) {
       console.log("POST");
       const options = makeOptions("POST", true, itemBody, loggedIn);
-      console.log("addEditItem POST url", MAINURL);
-      console.log("addEditItem options",options);
-      const data = await fetch(MAINURL, options).then(handleHttpErrors);
+      console.log("addEditItem POST url", MAINURL + extraURL);
+      console.log("addEditItem options", options);
+      const data = await fetch(MAINURL + extraURL, options).then(handleHttpErrors);
       return data;
     } else {
       console.log("PUT");
       const options = makeOptions("PUT", true, itemBody, loggedIn);
-      console.log("addEditItem PUT url", MAINURL + "/" + itemBody.body.id);
-      console.log("addEditItem options",options);
-      const data = await fetch(MAINURL + "/" + itemBody.id, options).then(handleHttpErrors);
+      console.log("addEditItem PUT url", MAINURL + extraURL);
+      console.log("addEditItem options", options);
+      const data = await fetch(MAINURL + extraURL, options).then(handleHttpErrors);
       return data;
     }
   };
 
-  async function deleteItem(id) {
+  async function deleteItem(loggedIn, extraURL) {
+    console.log("deleteItem loggedIn", loggedIn);
+    console.log("deleteItem url", MAINURL + extraURL);
     const options = makeOptions("DELETE");
-    const data = await fetch(MAINURL + "/" + id, options).then(handleHttpErrors);
+    const data = await fetch(MAINURL + extraURL, options).then(handleHttpErrors);
     return data;
   };
 
-function putFunc() {
-    console.log("put function");
-    let options = {
-        method: "PUT",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "id": 2,
-            "firstName": "Hans",
-            "lastName": "Pan",
-            "phone": "545555555"
-        })
-    }
-
-    fetch("http://localhost:8080/the-facade-and-the-matching-endpoints/api/person/2", options);
-};
-
-function deleteFunc() {
-    console.log("delete function");
-    let options = {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-
-    fetch("http://localhost:8080/the-facade-and-the-matching-endpoints/api/person/3", options);
-};
-
-
   return {
-    getData,
-    getDataAsync,
     addEditItem,
     deleteItem
   };
--- Course-Assignment-3_Client/react_security/src/components/Hobbies.js
+++ Course-Assignment-3_Client/react_security/src/components/Hobbies.js
@@ -8,29 +8,43 @@ function Hobbies(props) {
 	console.log('Hobbies apiFacade', props.apiFacade);
 	console.log('Hobbies loggedIn', props.loggedIn);
 
-	const [ hobbyID, setHobbyID ] = useState(0);
-	const [ hobbyName, setHobbyName ] = useState('');
-	const [ description, setDescription ] = useState('');
-	console.log('Hobbies hobbies', props.hobbies);
-	console.log('Hobbies hobbyName', hobbyName);
-	console.log('Hobbies description', description);
+	const [hobbyID, setHobbyID] = useState(0);
+	const [hobbyName, setHobbyName] = useState('');
+	const [description, setDescription] = useState('');
+	const [hobbies, setHobbies] = useState([]);
+	console.log('Hobbies hobbies', hobbies);
 
 	const newHobby = (evt) => {
 		console.log('newHobby');
 		let itemBody = {
-			body: JSON.stringify({
-				id: 0,
-				name: hobbyName,
-				description: description
-			})
-		};
+			hobbyID: 0,
+			name: hobbyName,
+			description: description
+		}
 		console.log('newHobby itemBody', itemBody);
-		props.apiFacade.addEditItem(itemBody, props.loggedIn);
+		props.apiFacade.addEditItem(itemBody, props.loggedIn, "/api/hobby/add", hobbyID);
+	};
+	const editHobby = (evt) => {
+		console.log('editHobby');
+		let itemBody = {
+			hobbyID: hobbyID,
+			name: hobbyName,
+			description: description
+		}
+		console.log('editHobby itemBody', itemBody);
+		props.apiFacade.addEditItem(itemBody, props.loggedIn, "/api/hobby/edit", hobbyID);
+	};
+	const deleteHobby = (evt) => {
+		console.log('deleteHobby');
+		props.apiFacade.deleteItem(props.loggedIn, "/api/hobby/delete/" + hobbyID, hobbyID);
 	};
 
 	const onChange = (evt) => {
 		console.log('onChange  ->', evt.target.id);
-		if ([ evt.target.id ] == 'hobbyname') {
+		if ([evt.target.id] == 'hobbyID') {
+			console.log('hobbyID', evt.target.value);
+			setHobbyID(Number(evt.target.value));
+		} else if ([evt.target.id] == 'hobbyname') {
 			console.log('hobbyName', evt.target.value);
 			setHobbyName(evt.target.value);
 		} else {
@@ -39,26 +53,12 @@ function Hobbies(props) {
 		}
 	};
 
-	// const onChange = (evt) => {
-	// 	console.log('onChange  ->', evt.target.id);
-	// 	if ([ evt.target.id ] == 'id') {
-	// 		console.log('hobbyName', evt.target.value);
-	// 		setHobbyName(evt.target.value);
-	// 	} else if ([ evt.target.id ] == 'hobbyname') {
-	// 		console.log('hobbyName', evt.target.value);
-	// 		setHobbyName(evt.target.value);
-	// 	} else {
-	// 		console.log('description', evt.target.value);
-	// 		setDescription(evt.target.value);
-	// 	}
-	// };
-
 	useEffect(() => {
 		const getData = async () => {
 			try {
 				const data = await loginFacade.fetchData('/api/hobby/hobbies');
 				console.log('data', data);
-				props.updateHobbies(data);
+				setHobbies(data);
 			} catch (e) {
 				console.log('err', e);
 			}
@@ -66,7 +66,7 @@ function Hobbies(props) {
 		getData();
 	}, []);
 
-	if (Array.isArray(props.hobbies) && props.hobbies[0] != undefined) {
+	if (Array.isArray(hobbies) && hobbies[0] != undefined) {
 		return (
 			<div>
 				<h2>Hobbies</h2>
@@ -80,7 +80,7 @@ function Hobbies(props) {
 							</tr>
 						</thead>
 						<tbody>
-							{props.hobbies.map((element, index) => (
+							{hobbies.map((element, index) => (
 								<tr key={uuid()}>
 									<td>{element.hobbyID}</td>
 									<td>{element.name}</td>
@@ -97,13 +97,27 @@ function Hobbies(props) {
 						<input placeholder="Description" id="hobbydescription" />
 					</form>
 				</div>
+				<div>
+					<form onChange={onChange}>
+						<button onClick={editHobby}>Edit Hobby</button>
+						<input placeholder="Hobby ID" id="hobbyID" />
+						<input placeholder="Hobby Name" id="hobbyname" />
+						<input placeholder="Description" id="hobbydescription" />
+					</form>
+				</div>
+				<div>
+					<form onChange={onChange}>
+						<button onClick={deleteHobby}>Delete Hobby</button>
+						<input placeholder="Hobby ID" id="hobbyID" />
+					</form>
+				</div>
 			</div>
 		);
 	} else {
 		return (
 			<div>
 				<h2>No Hobbies yet</h2>
-				{props.hobbies.msg}
+				{hobbies.msg}
 			</div>
 		);
 	}
