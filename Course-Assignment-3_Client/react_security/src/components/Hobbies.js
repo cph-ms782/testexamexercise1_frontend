73a04f5739d47528ce17f3dcc0492487675c1a98
Martin <cph-ms782@cphbusiness.dk>
2020-01-11 14:57:49 +0100

    create person (incl. auto city fetching)

--- Course-Assignment-3_Client/react_security/src/components/Hobbies.js
+++ Course-Assignment-3_Client/react_security/src/components/Hobbies.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 // import MAINURL from "../settings";
 import loginFacade from './loginFacade';
+import postNumbers from '../postNumbers.json';
 import uuid from 'uuid/v1';
 
 function Hobbies(props) {
 	console.log('Hobbies');
-	console.log('Hobbies apiFacade', props.apiFacade);
+	// console.log('Hobbies apiFacade', props.apiFacade);
 	console.log('Hobbies loggedIn', props.loggedIn);
 
-	const [hobbyID, setHobbyID] = useState(0);
-	const [hobbyName, setHobbyName] = useState('');
-	const [description, setDescription] = useState('');
-	const [hobbies, setHobbies] = useState([]);
+	const [ hobbyID, setHobbyID ] = useState(0);
+	const [ hobbyName, setHobbyName ] = useState('');
+	const [ description, setDescription ] = useState('');
+	const [ hobbies, setHobbies ] = useState([]);
+	const [ personID, setPersonID ] = useState(0);
+	const [ email, setEmail ] = useState('');
+	const [ firstName, setFirstName ] = useState('');
+	const [ lastName, setLastName ] = useState('');
+	const [ street, setStreet ] = useState('');
+	const [ additionalInfo, setAdditionalInfo ] = useState('');
+	const [ zipCode, setZipCode ] = useState(0);
+	const [ city, setCity ] = useState('');
 	console.log('Hobbies hobbies', hobbies);
 
 	const newHobby = (evt) => {
@@ -20,9 +29,24 @@ function Hobbies(props) {
 			hobbyID: 0,
 			name: hobbyName,
 			description: description
-		}
+		};
 		console.log('newHobby itemBody', itemBody);
-		props.apiFacade.addEditItem(itemBody, props.loggedIn, "/api/hobby/add", hobbyID);
+		props.apiFacade.addEditItem(itemBody, props.loggedIn, '/api/hobby/add', hobbyID);
+	};
+	const newPerson = (evt) => {
+		console.log('newPerson');
+		let itemBody = {
+			personID: 0,
+			email: email,
+			firstName: firstName,
+			lastName: lastName,
+			street: street,
+			additionalInfo: additionalInfo,
+			zipCode: zipCode,
+			city: city
+		};
+		console.log('newPerson itemBody', itemBody);
+		props.apiFacade.addEditItem(itemBody, props.loggedIn, '/api/person/add', personID);
 	};
 	const editHobby = (evt) => {
 		console.log('editHobby');
@@ -30,26 +54,73 @@ function Hobbies(props) {
 			hobbyID: hobbyID,
 			name: hobbyName,
 			description: description
-		}
+		};
 		console.log('editHobby itemBody', itemBody);
-		props.apiFacade.addEditItem(itemBody, props.loggedIn, "/api/hobby/edit", hobbyID);
+		props.apiFacade.addEditItem(itemBody, props.loggedIn, '/api/hobby/edit', hobbyID);
 	};
 	const deleteHobby = (evt) => {
 		console.log('deleteHobby');
-		props.apiFacade.deleteItem(props.loggedIn, "/api/hobby/delete/" + hobbyID, hobbyID);
+		props.apiFacade.deleteItem(props.loggedIn, '/api/hobby/delete/' + hobbyID, hobbyID);
 	};
 
 	const onChange = (evt) => {
 		console.log('onChange  ->', evt.target.id);
-		if ([evt.target.id] == 'hobbyID') {
+		if ([ evt.target.id ] == 'hobbyID') {
 			console.log('hobbyID', evt.target.value);
 			setHobbyID(Number(evt.target.value));
-		} else if ([evt.target.id] == 'hobbyname') {
+		} else if ([ evt.target.id ] == 'hobbyname') {
 			console.log('hobbyName', evt.target.value);
 			setHobbyName(evt.target.value);
 		} else {
 			console.log('description', evt.target.value);
 			setDescription(evt.target.value);
+		}
+	};
+	const onChangePerson = (evt) => {
+		console.log('onChangePerson  ->', evt.target.id);
+		switch (evt.target.id) {
+			case 'personID':
+				console.log('personID', evt.target.value);
+				setPersonID(Number(evt.target.value));
+				break;
+			case 'email':
+				console.log('email', evt.target.value);
+				setEmail(evt.target.value);
+				break;
+			case 'firstName':
+				console.log('firstName', evt.target.value);
+				setFirstName(evt.target.value);
+				break;
+			case 'lastName':
+				console.log('lastName', evt.target.value);
+				setLastName(evt.target.value);
+				break;
+			case 'street':
+				console.log('street', evt.target.value);
+				setStreet(evt.target.value);
+				break;
+			case 'additionalInfo':
+				console.log('additionalInfo', evt.target.value);
+				setAdditionalInfo(evt.target.value);
+				break;
+			case 'zipCode':
+				console.log('zipCode', evt.target.value);
+				setZipCode(evt.target.value);
+				console.log("zipCode zipCode", zipCode);
+				const cityName = postNumbers.find((postNumber, index) => {
+					return postNumber.nr == evt.target.value;
+				});
+				if (cityName != undefined) {
+					console.log('cityName.navn', cityName.navn);
+					setCity(cityName.navn);
+				} else {
+					console.log('cityName undefined');
+					setCity("");
+				}
+				break;
+
+			default:
+				console.log('default, doing nothing,', evt.target.value);
 		}
 	};
 
@@ -80,7 +151,7 @@ function Hobbies(props) {
 							</tr>
 						</thead>
 						<tbody>
-							{hobbies.map((element, index) => (
+							{hobbies.map((element) => (
 								<tr key={uuid()}>
 									<td>{element.hobbyID}</td>
 									<td>{element.name}</td>
@@ -109,6 +180,19 @@ function Hobbies(props) {
 					<form onChange={onChange}>
 						<button onClick={deleteHobby}>Delete Hobby</button>
 						<input placeholder="Hobby ID" id="hobbyID" />
+					</form>
+				</div>
+				<div>
+					<form onChange={onChangePerson}>
+						<button onClick={newPerson}>New Person</button>
+						{/* <input placeholder="Person ID" id="personID" /> */}
+						<input placeholder="email" id="email" />
+						<input placeholder="firstName" id="firstName" />
+						<input placeholder="lastName" id="lastName" />
+						<input placeholder="street" id="street" />
+						<input placeholder="additionalInfo" id="additionalInfo" />
+						<input placeholder="zipCode" id="zipCode"/>
+						<input placeholder="city" id="city" value={city}/>
 					</form>
 				</div>
 			</div>
